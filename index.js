dfc4e1a126e9e31fd20db12a7f83787406bbc8e6
Imogen Hardy <imogen.hardy@gmail.com>
2018-11-17 18:33:53 +0000

    Initial registry checking

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const fetch = require('node-fetch');
-const searchArgs = process.argv.slice(2);
+const chalk = require('chalk');
+const searchArgs = process.argv.slice(2, 10);
 
-console.log(searchArgs)
+const npmUrl = 'https://registry.npmjs.org/';
+const thesaurusUrl = 'https://api.datamuse.com/words?ml=';
+
+async function checkRegistry(packageName) {
+  const res = await fetch(`${npmUrl}${packageName}`);
+  if (res.status !== 200) {
+    return chalk.green(`${packageName} sounds like a good name!`);
+  }
+  return chalk.red(`${packageName} is already taken :(`);
+}
+
+async function searchAllArgs() {
+  for (const word of searchArgs) {
+    checkRegistry(word).then(console.log);
+  }
+}
+
+searchAllArgs();
